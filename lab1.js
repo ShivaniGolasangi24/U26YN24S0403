42810fe5fc49eefbc0552919dc4b66a91eee1072
ShivaniGolasangi24 <golasangishivani47@gmail.com>
2026-08-12 11:04:05 +0530

    lab1 with error

---
+++ lab1.js
@@ -0,0 +1,8 @@
+const http=require("http")
+const server=http.createServer((req,resp)=>{
+    resp.writeHead(200,{"content-type":"text/html"})
+    resp.write(`<h1>BLDEAS commerce,BHS Arts and TGP science college</h1>`)
+    resp.write(`<h2>Started in year 1963</h2>`)
+    resp.write(`<h3>Located in jamkhandi</h3>`)
+})
+server.listen(3000,()=>(console.log("server has been started")))
